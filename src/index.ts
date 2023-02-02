9d0068458c9a4119dbf6ac288e7771f676dbbe4e
CarlosRuiz <carua97@gmail.com>
2023-02-02 21:06:49 +0100

    Add files with the serie

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
 /* eslint-disable no-new */
-import { Footer } from './components/footer/footer';
+//import { Footer } from './components/footer/footer';
 import { Header } from './components/1.header/header';
 import { Menu } from './components/menu/menu';
-import { Tasks } from './components/tasks/tasks';
-import './index.scss';
-import { TaskStorageRepo } from './services/repository/task.storage.repo';
+//import { Tasks } from './components/tasks/tasks';
+//import { TaskStorageRepo } from './services/repository/task.storage.repo';
+import { Series } from './components/2.series/series';
+import { SeriesPending } from './components/3.serie-pending/series-pending';
+import { SeriesWatched } from './components/4.serie-watched/series-watched';
 
 export type MenuOption = {
   label: string;
   path: string;
 };
 
-const menuOptions: MenuOption[] = [
-  { label: 'Inicio', path: '/home' },
-  { label: 'Tares', path: '/tasks' },
-  { label: 'Acerca de', path: '/about.html' },
-];
+new Header('.container');
+new Series('.main');
+new SeriesPending('.series');
+new SeriesWatched('.series');
 
-console.log('Load', location.pathname);
-new Header('#root');
-new Menu('.header', menuOptions);
-if (location.pathname === '/home') {
-  new Tasks('main', new TaskStorageRepo());
-}
+// console.log('Load', location.pathname);
+// new Header('#root');
+// new Menu('.header', menuOptions);
+// if (location.pathname === '/home') {
+//   new Tasks('main', new TaskStorageRepo());
+// }
 
-new Footer('#root');
+// new Footer('#root');
